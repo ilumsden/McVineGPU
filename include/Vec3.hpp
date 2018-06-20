ec1bd323b1b67d98ee9faa3a165c3344bd91334d
Ian Lumsden <lumsden.ian@gmail.com>
2018-06-20 10:44:37 -0400

    Adds some new functions to the Vec3 class.

--- include/Vec3.hpp
+++ include/Vec3.hpp
@@ -14,12 +14,17 @@ class Vec3
 {
     public:
         __host__ __device__ Vec3(T a=T(), T b=T(), T c=T());
+        __host__ __device__ Vec3(const Vec3<T> &b);
+        __host__ __device__ const Vec3<T> & operator=(const Vec3<T> &b);
         // Default destructor for Vec3
         __host__ __device__ ~Vec3() { ; }
         // Returns the size of the vector (always 3)
         __host__ __device__ std::size_t size() { return 3; }
         /* These three functions are simple "getter" functions
          * that return the values of x, y, and z respectively.
+         * They return the values by-value, so they should
+         * ideally only be used for simpler types (i.e int,
+         * float, double, etc.).
          */
         __host__ __device__ T getX() const { return x; }
         __host__ __device__ T getY() const { return y; }
@@ -41,9 +46,11 @@ class Vec3
         __host__ __device__ Vec3<T> operator*(const Vec3<T> &b) const;
         __host__ __device__ void operator*=(const T n);
         __host__ __device__ void operator*=(const Vec3<T> &b);
+        __host__ __device__ T operator|(const Vec3<T> &b) const;
+        __host__ __device__ bool operator==(const Vec3<T> &b) const;
+        __host__ __device__ bool operator!=(const Vec3<T> &b) const;
         __host__ __device__ const T & operator[](int i) const;
         __host__ __device__ T & operator[](int i);
-        __host__ __device__ T operator|(const Vec3<T> &b) const;
     private:
         /* x, y, and z reference the three elements of the m_data
          * array. They are references, and, as such, any changes
@@ -61,7 +68,7 @@ class Vec3
  * This function is able to run on both host (CPU) and device (GPU).
  */
 template <typename T>
-__host__ __device__ Vec3::Vec3(T a=T(), T b=T(), T c=T())
+__host__ __device__ Vec3<T>::Vec3(T a=T(), T b=T(), T c=T())
     : x(m_data[0]), y(m_data[1]), z(m_data[2])
 {
     x = a;
@@ -69,19 +76,45 @@ __host__ __device__ Vec3::Vec3(T a=T(), T b=T(), T c=T())
     z = c;
 }
 
+/* The copy constructor for Vec3.
+ * This function copies the contents of Vec3 object b
+ * into the newly created Vec3 object.
+ */
+template <typename T>
+__host__ __device__ Vec3<T>::Vec3(const Vec3<T> &b)
+    : x(m_data[0]), y(m_data[1]), z(m_data[2])
+{
+    x = b[0];
+    y = b[1];
+    z = b[2];
+}
+
+/* This function is an overloaded assignment operator to
+ * allowed for copying of a Vec3 object to another existing
+ * Vec3 object.
+ */
+template <typename T>
+__host__ __device__ const Vec3<T> & operator=(const Vec3<T> &b)
+{
+    x = b[0];
+    y = b[1];
+    z = b[2];
+    return *this;
+}
+
 /* This function normalizes the vector.
  * All that it does is multiplies each element in the Vec3
  * object by 1/length, where length is the length of the vector.
  */
 template <typename T>
-__host__ __device__ void Vec3::normalize()
+__host__ __device__ void Vec3<T>::normalize()
 {
     *this *= (1.0 / this->length());
 }
 
 // This function calculates and returns the length of the vector.
 template <typename T>
-__host__ __device__ T Vec3::length() const
+__host__ __device__ T Vec3<T>::length() const
 {
     return std::sqrt( std::abs(x)*std::abs(x) + std::abs(y)*std::abs(y) + std::abs(z)*std::abs(z) );
 }
@@ -90,36 +123,36 @@ __host__ __device__ T Vec3::length() const
  * stores the result in a new Vec3 object, which is returned.
  */
 template <typename T>
-__host__ __device__ Vec3<T> Vec3::operator+(const Vec3<T> &b) const
+__host__ __device__ Vec3<T> Vec3<T>::operator+(const Vec3<T> &b) const
 {
-    return Vec3<T>(x+b.getX(), y+b.getY(), z+b.getZ());
+    return Vec3<T>(x+b[0], y+b[1], z+b[2]);
 }
 
 /* This function performs element-wise addition on two vectors
  * and stores the result in the Vec3 object on the left of the operator.
  */
 template <typename T>
-__host__ __device__ void Vec3::operator+=(const Vec3<T> &b)
+__host__ __device__ void Vec3<T>::operator+=(const Vec3<T> &b)
 {
-    x += b.getX();
-    y += b.getY();
-    z += b.getZ();
+    x += b[0];
+    y += b[1];
+    z += b[2];
 }
 
 /* This function performs element-wise substraction on two vectors
  * and stores the result in a new Vec3 object, which is returned.
  */
 template <typename T>
-__host__ __device__ Vec3<T> Vec3::operator-(const Vec3<T> &b) const
+__host__ __device__ Vec3<T> Vec3<T>::operator-(const Vec3<T> &b) const
 {
-    return Vec3<T>(x-b.getX(), y-b.getY(), z-b.getZ());
+    return Vec3<T>(x-b[0], y-b[1], z-b[2]);
 }
 
 /* This function negates each component of the vector and
  * stores the resulting vector in a new Vec3 object, which is returned.
  */
 template <typename T>
-__host__ __device__ Vec3<T> Vec3::operator-() const
+__host__ __device__ Vec3<T> Vec3<T>::operator-() const
 {
     return Vec3<T>(-x, -y, -z);
 }
@@ -128,18 +161,18 @@ __host__ __device__ Vec3<T> Vec3::operator-() const
  * and stores the result in the Vec3 object on the left of the operator.
  */
 template <typename T>
-__host__ __device__ void Vec3::operator-=(const Vec3<T> &b) const
+__host__ __device__ void Vec3<T>::operator-=(const Vec3<T> &b) const
 {
-    x -= b.getX();
-    y -= b.getY();
-    z -= b.getZ();
+    x -= b[0];
+    y -= b[1];
+    z -= b[2];
 }
 
 /* This function performs a scalar product on the vector and
  * stores the resulting vector in a new Vec3 object, which is returned.
  */
 template <typename T>
-__host__ __device__ Vec3<T> operator*(const T n) const
+__host__ __device__ Vec3<T> Vec3<T>::operator*(const T n) const
 {
     return Vec3<T>(n*x, n*y, n*z);
 }
@@ -148,18 +181,18 @@ __host__ __device__ Vec3<T> operator*(const T n) const
  * and stores the resulting vector in a new Vec3 object, which is returned.
  */
 template <typename T>
-__host__ __device__ Vec3<T> operator*(const Vec3<T> &b) const
+__host__ __device__ Vec3<T> Vec3<T>::operator*(const Vec3<T> &b) const
 {
-    return Vec3<T>(y*b.getZ() - z*b.getY(),
-                   z*b.getX() - x*b.getZ(),
-                   x*b.getY() - y*b.getX());
+    return Vec3<T>(y*b[2] - z*b[1],
+                   z*b[0] - x*b[2],
+                   x*b[1] - y*b[0]);
 }
 
 /* This function performs a scalar product on the vector and
  * stores the resulting vector in the vector used for the operation.
  */
 template <typename T>
-__host__ __device__ void operator*=(const T n)
+__host__ __device__ void Vec3<T>::operator*=(const T n)
 {
     x *= n;
     y *= n;
@@ -170,11 +203,40 @@ __host__ __device__ void operator*=(const T n)
  * and stores the result in the Vec3 object on the left of the operator.
  */
 template <typename T>
-__host__ __device__ void operator*=(const Vec3<T> &b)
+__host__ __device__ void Vec3<T>::operator*=(const Vec3<T> &b)
 {
-    x = y*b.getZ() - z*b.getY();
-    y = z*b.getX() - x*b.getZ();
-    z = x*b.getY() - y*b.getX();
+    x = y*b[2] - z*b[1];
+    y = z*b[0] - x*b[2];
+    z = x*b[1] - y*b[0];
+}
+
+/* This function calculates the dot product of two vectors and
+ * returns the result.
+ */
+template <typename T>
+__host__ __device__ T Vec3<T>::operator|(const Vec3<T> &b) const
+{
+    return x*b[0] + y*b[1] + z*b[2];
+}
+
+/* This function is a basic comparison operator that returns true
+ * if the two vectors are equal (components are equal) and false
+ * otherwise.
+ */
+template <typename T>
+__host__ __device__ bool Vec3<T>::operator==(const Vec3<T> &b) const
+{
+    return (x == b[0]) && (y == b[1]) && (z == b[2]);
+}
+
+/* This function is a basic comparison operator that returns true
+ * if the two vectors are not equal (components are not equal) and
+ * false otherwise.
+ */
+template <typename T>
+__host__ __device__ bool Vec3<T>::operator!=(const Vec3<T> &b) const
+{
+    return (x != b[0]) || (y != b[1]) || (z != b[2]);
 }
 
 /* This function is an alternate "getter" function that uses array
@@ -184,15 +246,15 @@ __host__ __device__ void operator*=(const Vec3<T> &b)
  * CPU or GPU.
  */
 template <typename T>
-__host__ __device__ const T & operator[](int i) const
+__host__ __device__ const T & Vec3<T>::operator[](int i) const
 {
     if (i < 3) { return m_data[i]; }
     else
     {
         fprintf(stderr, "Vec3: Index %i out of bounds.\n", i);
 /* If the error occurs on the GPU, the PTX assembly command "trap"
- * will be injected, causing the GPU code to immediately halt.
- * This will also call the kernel to return a cudaErrorUnknown
+ * will be injected, causing the GPU code to immediately end.
+ * This will also cause the kernel to return a cudaErrorUnknown
  * (errcode 30).
  */
 #if defined(__CUDA_ARCH__)
@@ -209,7 +271,7 @@ __host__ __device__ const T & operator[](int i) const
  * except that it returns a NON-CONST reference to the data accessed.
  */
 template <typename T>
-__host__ __device__ T & operator[](int i)
+__host__ __device__ T & Vec3<T>::operator[](int i)
 {
     if (i < 3) { return m_data[i]; }
     else
@@ -221,15 +283,6 @@ __host__ __device__ T & operator[](int i)
         throw std::out_of_range("out of bounds");
 #endif
     }
-}
-
-/* This function calculates the dot product of two vectors and
- * returns the result.
- */
-template <typename T>
-__host__ __device__ T operator|(const Vec3<T> &b) const
-{
-    return x*b.getX() + y*b.getY() + z*b.getZ();
 }
 
 #endif
