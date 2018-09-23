dc6ee41d7c024adf46eef8035682786f06141738
Ian Lumsden <lumsden.ian@gmail.com>
2018-09-23 19:31:07 -0400

    First, untested version of the ConstantQEScatterer class.

---
+++ include/ConstantQEScatterer.hpp
@@ -0,0 +1,42 @@
+#ifndef ISO_SCATTER_HPP
+#define ISO_SCATTER_HPP
+
+#include "AbstractScatterer.hpp"
+
+namespace mcvine
+{
+
+    namespace gpu
+    {
+
+        namespace scatter
+        {
+
+            class ConstantQEScatterer : public AbstractScatterer
+            {
+                public:
+                    ConstantQEScatterer(std::shared_ptr<Beam> b,
+                                        std::shared_ptr<AbstractShape> s,
+                                        const float kQ, const float kE)
+                    : AbstractScatterer(b, s), Q(kQ), E(kE) { type = 1; }
+
+                    virtual ~ConstantQEScatterer() { ; }
+
+                    virtual void scatter() override;
+
+                protected:
+
+                    virtual void findScatteringVels() override;
+
+                private:
+
+                    const float Q, E;
+            };
+
+        }
+
+    }
+
+}
+
+#endif
